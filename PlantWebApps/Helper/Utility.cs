1a4623254ad280631c01fa1250f6940a564e78d0
Muhammad Setiawan <MISetiawan@thiess.com>
2023-11-30 08:56:52 +0800

    30.11.2023

--- PlantWebApps/Helper/Utility.cs
+++ PlantWebApps/Helper/Utility.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Data;
 using System.Drawing;
 
@@ -143,6 +144,42 @@ namespace PlantWebApps.Helper
                 return " WHERE " + F.Substring(4);
             else
                 return string.Empty;
+        }
+
+        public static string GetStringOrNull(object value)
+        {
+            return value != DBNull.Value ? (string)value : null;
+        }
+
+        public static DataTable getDataTable(String qTxt)
+        {
+            var dataTable = SQLFunction.execQuery(qTxt);
+
+            return dataTable;
+        }
+
+        public static ArrayList genDataTable(DataTable dt, Dictionary<String, String> sCol, String IDKey = "")
+        {
+            DataTable dt2 = new DataTable();
+            ArrayList arrayList = new ArrayList();
+
+            foreach (DataRow rows in dt.Rows)
+            {
+                Dictionary<string, string> dt3 = new Dictionary<string, string>();
+                foreach (var col in sCol)
+                {
+                    if (col.Key == "#")
+                    {
+                        dt3.Add("#", col.Value.Replace("<id>", rows[IDKey].ToString()));
+                    }
+                    else
+                    {
+                        dt3.Add(col.Value, rows[col.Key].ToString());
+                    }
+                }
+                arrayList.Add(dt3);
+            }
+            return arrayList;
         }
     }
 }
---
+++ PlantWebApps/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PlantWebApps.Models
+{
+    public class UserViewModel
+    {
+        public string Data { get; set; }
+        public PaginationInfo Pagination { get; set; }
+    }
+
+    public class PaginationInfo
+    {
+        public int TotalRecords { get; set; }
+        public int TotalPages { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
