bf89d0a25f71f165bc66019c7e65731da92bb185
Pahri <muhammadpahrizal790@gmail.com>
2023-12-11 15:37:00 +0800

    11.12.2023.1

---
+++ PlantWebApps/Controllers/JobDispatch/JobDispatchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlantWebApps.Controllers.JobDispatch
+{
+    public class JobDispatchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/JobDispatch/Index.cshtml");
+        }
+        public IActionResult loadData()
+        {
+            return null;
+        }
+    }
+}
