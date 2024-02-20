9e5c1b91f2714e4b6390b6bb2167a1f3a4abda7a
Pahri <muhammadyulasfi@students.universitasmulia.ac.id>
2024-02-20 09:12:02 +0800

    .

--- PlantWebApps/Controllers/PER/ExrRepairJobHistory/PrintItem.cs
+++ PlantWebApps/Controllers/PER/ExrRepairJobHistory/PrintItem.cs
@@ -134,7 +134,7 @@ namespace PlantWebApps.Controllers.PER.ExrRepairJobHistory
         }
         public IActionResult AfterRepairDirect(string value)
         {
-            string query = "SELECT * FROM v_ExrRepairTagx WHERE ID IN (" + value + ")";
+            string query = "SELECT *, CONVERT(varchar, ReceivedDate, 103) AS FormatDate FROM v_ExrRepairTagx WHERE ID IN (" + value + ")";
             Console.WriteLine(query);
 
             var dataTable = SQLFunction.execQuery(query);
@@ -149,13 +149,44 @@ namespace PlantWebApps.Controllers.PER.ExrRepairJobHistory
                 foreach (DataColumn column in dataTable.Columns)
                 {
                     rowData[column.ColumnName] = row[column];
+
+                    var eid = rowData["ID"];
+                    Console.WriteLine("console" + eid);
+
+                    if (eid != "")
+                    {
+                        string dataQuery = $"select concat('Parent WO ',OffsiteWO,', ','Offsite WO ',ChildWO,',','Internal WO ',IntWO,', ','PN ','(',TCIPartNo,')',', ','Description1',', ','OP ','<',OPNONew,'>',', ','Unit No ','[',UnitNumber,'] ', 'Unit Desc ','@',UnitDescription,'#') as Combine from v_ExrRepairTagx where ID= {Utility.Evar(eid, 0)}";
+
+                        var dataResult = SQLFunction.execQuery(dataQuery);
+                        Console.WriteLine("data result is " + dataQuery);
+                        var combineValues = new List<string>();
+
+                        foreach (DataRow dataRow in dataResult.Rows)
+                        {
+                            combineValues.Add(dataRow["Combine"].ToString());
+                        }
+
+                        rowData["DataValue"] = combineValues;
+
+                        Console.WriteLine("is id");
+                    }
+                    else
+                    {
+                        string dataQuery = @"select concat('Parent WO ',OffsiteWO,', ','Offsite WO ',ChildWO,', ','Internal WO ',IntWO,', ','PN ','(',TCIPartNo,')',', ','Description1',', ','OP ','<',OPNONew,'>',', ','Unit No ','[',UnitNumber,'] ', 'Unit Desc ','@',UnitDescription,'#') as Combine from v_ExrRepairTagx where ID=325";
+
+                        var dataResult = SQLFunction.execQuery(dataQuery);
+                        var dataRow = dataResult.Rows[0];
+                        rowData["DataValue"] = dataRow["Combine"];
+
+                        Console.WriteLine("is not id");
+                    }
                 }
 
                 dataList.Add(dataItem);
             }
             ViewBag.data = dataList;
 
-            return View("~/Views/PER/ExrRepairJobHistory/PrintItem/AfterRepairStandard.cshtml");
+            return View("~/Views/PER/ExrRepairJobHistory/PrintItem/AfterRepairDirect.cshtml");
         }
         public IActionResult PrintHoldItem()
         {
